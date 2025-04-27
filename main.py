5b7b9cbe9073f063e16c38d99ead3a841ad123ec
despot707 <despot707@gmail.com>
2025-04-26 22:42:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
-
-from src import bot
 from dotenv import load_dotenv
 from g4f.cookies import set_cookies
 
+# Bring in the new command registration and the client instance:
+from src.bot import register_commands
+from src.aclient import discordClient
 
-if __name__ == '__main__':
-    set_cookies(".bing.com", {
-    "_U": str(os.getenv("BING_COOKIE"))
-    })
-    set_cookies(".google.com", {
-    "__Secure-1PSID": str(os.getenv("GOOGLE_PSID"))
-    })
-    bot.run_discord_bot()
+load_dotenv()
+
+if __name__ == "__main__":
+    # if you still need Bing/Google cookies for g4f:
+    set_cookies(".bing.com",     {"_U": os.getenv("BING_COOKIE", "")})
+    set_cookies(".google.com",   {"__Secure-1PSID": os.getenv("GOOGLE_PSID", "")})
+
+    # wire up all your slash commands
+    register_commands()
+    # finally start the bot
+    discordClient.run(os.getenv("DISCORD_BOT_TOKEN"))
